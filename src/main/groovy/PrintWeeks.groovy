4d44580cdca11ba78a557f5954da62da9e70e9b1
Thomas Auinger <thomas@auinger.de>
2022-06-23 10:51:15 +0200

    comments

--- src/main/groovy/PrintWeeks.groovy
+++ src/main/groovy/PrintWeeks.groovy
@@ -3,16 +3,20 @@ import java.time.LocalDate
 import java.time.temporal.TemporalAdjusters
 import java.time.temporal.WeekFields;
 
+// prep stuff
 def forYear = LocalDate.now().year
 weekField = WeekFields.of(Locale.GERMANY).weekOfWeekBasedYear();
 
+// start with Monday of the first week of the year (sometimes the first day is in the last week of prev year)
 def date = LocalDate.of(forYear, 1, 1).withDayOfMonth(1);
 date = date.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
 if (getWeekOfYear(date) > 1) date = date.plusWeeks(1)
 
+// some output
 def currentYear = date.year
 println("Dumping all weeks for year $currentYear")
 
+// loop until we have moved into the next year
 while (date.year == currentYear) {
     def lastDayOfWeek = date.plusDays(6)
     println("KW ${getWeekOfYear(date)}: ${formatDate(date)} - ${formatDate(lastDayOfWeek, true)}")
@@ -20,11 +24,15 @@ while (date.year == currentYear) {
 }
 
 
+//
+// ---- helper functions ---
+//
+
 def getWeekOfYear(LocalDate date) {
     return date.get(weekField)
 }
 
-def formatDate(LocalDate date, boolean includeYear=false) {
+def formatDate(LocalDate date, boolean includeYear = false) {
     if (includeYear) return date.format("dd MMM yyyy")
     else return date.format("dd MMM")
 }
